875262e7134f1fd1f239faf5f70b52af053719af
Rishit Epari <epari.rishit@gmail.com>
2019-10-05 18:12:20 +0530

    Added chnages related to movie and customer details.

---
+++ VidlyRental/Controllers/CustomersController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyRental.Models;
+
+namespace VidlyRental.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customer
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer(){ Id= 1, Name="Harry"},
+                new Customer(){ Id= 2, Name="Dumbledore" },
+                new Customer(){ Id= 3, Name="Voldemort" },
+                new Customer(){ Id= 4, Name="Snape" },
+                new Customer(){ Id= 5, Name="Sirius" },
+                new Customer(){ Id= 6, Name="Hermione" },
+                new Customer(){ Id= 7, Name="Ron" },
+                new Customer(){ Id= 8, Name="Draco" },
+                new Customer(){ Id= 9, Name="Hagrid" },
+                new Customer(){ Id= 10, Name="Neville" },
+                new Customer(){ Id= 11, Name="Dobby" },
+                new Customer(){ Id= 12, Name="Moody" },
+                new Customer(){ Id= 13, Name="Lupin" },
+                new Customer(){ Id= 14, Name="Bellatrix" },
+                new Customer(){ Id= 15, Name="McGonagall" },
+                new Customer(){ Id= 16, Name="Newt Scamander" },
+                new Customer(){ Id= 17, Name="Grindelwald" },
+                new Customer(){ Id= 18, Name="Tina" },
+                new Customer(){ Id= 19, Name="Queenie" },
+                new Customer(){ Id= 20, Name="Jacob" }
+            };
+
+        }
+    }
+}
---
+++ VidlyRental/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyRental.Models;
+
+namespace VidlyRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie(){Id=1, Name="Harry Potter and the Philosopher's Stone"},
+                new Movie(){Id=2, Name="Harry Potter and the Chamber of Secrets"},
+                new Movie(){Id=3, Name="Harry Potter and the Prisoner of Azkaban"},
+                new Movie(){Id=4, Name="Harry Potter and the Goblet of Fire"},
+                new Movie(){Id=5, Name="Harry Potter and the Order of the Phoenix"},
+                new Movie(){Id=6, Name="Harry Potter and the Half-Blood Prince"},
+                new Movie(){Id=7, Name="Harry Potter and the Deathly Hallows – Part 1"},
+                new Movie(){Id=8, Name="Harry Potter and the Deathly Hallows – Part 2"}
+
+            };
+        }
+       
+    }
+}
