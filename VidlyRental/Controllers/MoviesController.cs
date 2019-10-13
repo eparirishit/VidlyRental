0b7d0f73844a99f1fe5120c94d9176c3f61dacc0
Rishit Epari <epari.rishit@gmail.com>
2019-10-13 23:31:33 +0530

    New/Edit form movies features

--- VidlyRental/Controllers/MoviesController.cs
+++ VidlyRental/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VidlyRental.Models;
+using VidlyRental.ViewModels;
 
 namespace VidlyRental.Controllers
 {
@@ -38,6 +39,52 @@ namespace VidlyRental.Controllers
             return View(customer);
         }
 
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public ActionResult New()
+        {
+            var genreList = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel()
+            {
+                Genres = genreList
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.Single(m => m.Id == id);
+
+            if (movie == null)
+                HttpNotFound();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
 
         private IEnumerable<Movie> GetMovies()
         {
